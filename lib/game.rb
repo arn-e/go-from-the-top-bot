6c77f1b6d4565abd130fc39c1ed0b45b6cf38e06
Apprentice <apprentice@devbootcamp.com>
2012-11-01 17:50:33 -0700

    fixed game - board integration bugs, updated spec removing mocked board object

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,9 @@
 require_relative 'player'
+require_relative 'board'
 
 class Game
 
+  attr_accessor :database, :board
   attr_reader :turn, :players, :date
 
 	def initialize(player_name_1,player_name_2,turn = "player 1")
@@ -9,6 +11,7 @@ class Game
     @turn = turn
     @players = []
     @winner = ''
+    @database='/Users/apprentice/Desktop/connect_four/db/game.db'
     add_player(player_name_1,player_name_2)
   end
 
@@ -21,18 +24,20 @@ class Game
   def place_attempt(column)
     if @board.valid_placement?(column)
       switch_turn
+      #add place chip
       true
     else
       false
     end
   end
 
-  def victory?(database='/Users/apprentice/Desktop/connect_four/db/game.db')
+  def victory?
     if @board.four_in_a_row?
       @winner = @turn
-      record_game(database)
+      record_game
       true
     end
+    false
   end
 
   def draw?
@@ -45,8 +50,8 @@ class Game
     end
   end
 
-  def record_game(database='/Users/apprentice/Desktop/connect_four/db/game.db')
-    db = SQLite3::Database.new(database)
+  def record_game
+    db = SQLite3::Database.new(@database)
     db.execute("INSERT INTO 'game_hist' (played_on,player_one,player_two,winner) 
                 VALUES (DATETIME('now'),?,?,?)", @players[0].name, @players[1].name, @winner)  
   end
@@ -58,7 +63,3 @@ class Game
   end
 end
 
-# Delete me later!
-class Board
-
-end
